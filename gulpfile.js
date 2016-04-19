09d8aef7a9ce41f2d0267dbccac35600a612eab0
Nazariii <nazarii.ivashkiv@gmail.com>
2016-04-19 16:25:02 +0300

    Add browserify for bundling

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,19 @@ var gulp = require('gulp');
 var connect = require('gulp-connect'); //runs local dev server
 var open = require('gulp-open'); // Open a UEL in a web browser
 
+var browserify = require('browserify'); // Bundles Js
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with gulp
+
+
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 
@@ -36,9 +43,20 @@ gulp.task('html', function () {
         .pipe(connect.reload());
 });
 
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle() // generate one file
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js')) // bundle name
+        .pipe(gulp.dest(config.paths.dist + '/scripts')) // bundle path
+        .pipe(connect.reload())
+});
+
 gulp.task('watch', function () {
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });
 
 //default gulp task, that runs html and open tasks by simply "gulp" in console
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,3 @@
+var App = console.log('Hi');
+
+module.exports = App;
