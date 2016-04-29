819c1851ffc49ae927186f6a49375f2facd76a50
Nazariii <nazarii.ivashkiv@gmail.com>
2016-04-29 15:03:34 +0300

    Add Link component and 404page

--- src/route.js
+++ src/route.js
@@ -3,12 +3,14 @@
 var React = require('react');
 var Router = require('react-router');
 
-var DefaultRouter = Router.DefaultRoute;
 var Route = Router.Route;
+var DefaultRouter = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRouter handler={require('./components/homePage')}/>
+        <NotFoundRoute handler={require('./components/page404')}/>
         <Route name="users" handler={require('./components/users/userPage')}/>
         <Route name="about" handler={require('./components/about/aboutPage')}/>
     </Route>
